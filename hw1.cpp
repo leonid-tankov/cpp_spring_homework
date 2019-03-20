29b1af1f2f98914ea8e5bd81840a4d9cc184b9b9
leonid213 <tankov.lenya@mail.ru>
2019-03-20 11:49:14 +0300

    Update2 hw1

--- hw1.cpp
+++ hw1.cpp
@@ -3,60 +3,66 @@
 
 using namespace std;
 
-bool is_prime( int a)
+bool is_prime(int a)
 {
-    if (a==1)
+    if (a == 1)
     return 0;
-    for (int i=2;i*i<=a;i++)
-        if (a%i==0)
+    for (int i = 2; i * i <= a; i++)
+        if (a % i == 0)
         return 0;
     return 1;
+}
+
+int find (int left, int right)
+{
+    int count = 0;
+    for (int j = left; j <= right; j++)
+        if (is_prime( Data[j]))
+            count++;
+    return count;
 }
 
 int main(int argc, char* argv[])
 {
     int count;
-    if (((argc-1) % 2 == 1) || (argc==1))
+    if (((argc-1) % 2 == 1) || (argc == 1))
         return -1;
-    for (int i=1;i<argc;i+=2)
+    for (int i = 1; i < argc; i += 2)
    {
-        count=0;
-        int m1=-1,m2=0;
-        if (atoi(argv[i])>atoi(argv[i+1]))
+        int left = -1, right = 0;
+        if (atoi(argv[i]) > atoi(argv[i+1]))
         {
-            cout<<0<<" ";
+            cout << 0 << " ";
             continue;
         }
-        int fl=0, fl2=0;
-        for (int j=0;j<Size;j++)
+        int flag = 0, flag2 = 0;
+        for (int j = 0; j < Size; j++)
         {
-            if (Data[j]>atoi(argv[i+1]))
+            if (Data[j] > atoi(argv[i+1]))
                 break;
-            if (m1==-1)
-                if (Data[j]==atoi(argv[i]))
+            if (left == -1)
+                if (Data[j] == atoi(argv[i]))
                 {
-                    fl++;
-                    m1=j;
+                    flag++;
+                    left = j;
                 }   
-            if (Data[j]==atoi(argv[i+1]))
+            if (Data[j] == atoi(argv[i+1]))
             {
-                fl2=1;
-                m2=j;
+                flag2 = 1;
+                right = j;
             }
         }
-        if (fl2==1)
-        fl++;
-        if (fl!=2)
-        cout <<0<<" ";           
+        if ( flag2 == 1 )
+            flag++;
+        if (flag != 2)
+            cout << 0 << " ";           
         else
         {
-            for (int j=m1; j<=m2; j++)
-                if (is_prime( Data[j]))
-                    count++;
-            if ((argc-2-i)==0)
-            cout <<count;
+            count = find(left, right);
+            if ((argc-2-i) == 0)
+                cout << count;
             else
-            cout <<count<<endl;
+                cout << count << endl;
         }
     }
     cout << endl;
